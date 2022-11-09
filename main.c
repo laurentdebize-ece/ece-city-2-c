1a3568a34cc289970c3e2aae3a87732c6a6368f6
maellecuenot <maelle.cuenot@edu.ece.fr>
2022-11-09 10:24:59 +0100

    version 2

--- main.c
+++ main.c
@@ -1,3 +1,22 @@
 //
 // Created by maell on 07/11/2022.
 //
+#include <stdio.h>
+#include "test.c"
+#include <time.h>
+#include <raylib.h>
+int main ()
+{
+    InitWindow(GetMonitorWidth(0), GetMonitorHeight(0), "Raylib > Allegro");
+
+
+    while (!WindowShouldClose()) {
+        float f = (float)GetScreenWidth() / (float)GetScreenHeight();
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Raylib > Allegro", 12 * f, 12 * f, 20 * f, BLACK);
+        DrawText("Popo > tout le monde", 30 * f, 30 * f, 30 * f, BLACK);
+        EndDrawing();
+    }
+    return 0;
+}
--- test.h
+++ test.h
@@ -4,5 +4,87 @@
 
 #ifndef ECE_CITY_2_C_TEST_H
 #define ECE_CITY_2_C_TEST_H
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct route
+{
+    int x;
+    int y;
+
+} route;
+
+typedef struct reseauEau
+{
+    int x;
+    int y;
+
+} reseauEau;
+typedef struct reseauElectricite
+{
+    int x;
+    int y;
+
+} reseauElectricite;
+
+typedef struct habitation
+{
+    int x;
+    int y;
+
+} habitation;
+
+typedef struct caserne
+{
+    int x;
+    int y;
+
+} caserne;
+
+typedef struct usineEau
+{
+    int x;
+    int y;
+
+} usineEau;
+
+typedef struct usineElectricite
+{
+    int x;
+    int y;
+
+} usineElectricite;
+
+typedef struct chemin
+{
+    route* route;
+    reseauEau* reseauEau;
+    reseauElectricite* reseauElectricite;
+
+} chemin;
+
+typedef struct bat
+{
+     habitation* habitation;
+
+} bat;
+
+typedef struct usines
+{
+    caserne* caserne;
+    usineEau* usineEau;
+    usineElectricite* usineElectricite;
+
+} usines;
+
+
+typedef struct InfoConstruction
+{
+    chemin* chemin;
+    bat* bat;
+    usines* usines;
+
+} InfoConstruction;
+
 
 #endif //ECE_CITY_2_C_TEST_H
